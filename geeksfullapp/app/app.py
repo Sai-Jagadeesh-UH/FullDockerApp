922e12a6eedc1cca3fa1fd3eb2a3fcc64cf5a8ee
Sai <ysaijagadeeshuh@gmail.com>
2024-03-20 00:43:09 -0500

    deleted unused files

--- geeksfullapp/app/app.py
+++ geeksfullapp/app/app.py
@@ -1,19 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask
 import os
-import sys
-from utils import logging
 # creating Flask Obj reference
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    logging.info(f'logging {os.getcwd()}')
-    print("\nI am printing")
-    print()
-    return os.getcwd()
+    return str(os.getcwd())+" Hai"
 
 
 if __name__ == '__main__':
-    index()
     app.run()
